7648c968683925373b08d371e346d61b2c12c036
JesusGH123 <a01423730@itesm.mx>
2022-04-15 19:48:01 -0500

    post completed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for, flash
+from audioop import cross
+from flask import Flask, jsonify, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 # Configure and get an instance of the db
 app.config['MYSQL_HOST'] = 'localhost'
@@ -16,22 +18,23 @@ db = MySQL(app)
 app.secret_key = 'secret_key'
 
 #Routes
-@app.route('/')
+@app.route('/', methods=['GET'])
 @cross_origin()
 def index():
     if request.method == 'GET':
         cur = db.connection.cursor()
         cur.execute('SELECT * FROM car')
         data = cur.fetchall() #Get the query in variable data
-        print(data)
     return jsonify([{'id':car[0],'brand': car[1], 'model': car[2], 'year': car[3]} for car in data])
 
 @app.route('/add_car', methods=['POST'])
+@cross_origin()
 def add_car():
     if request.method == 'POST':
-        brand = request.form['brand']
-        model = request.form['model']
-        year = request.form['year']
+        data = request.get_json()
+        brand = data["brand"]
+        model = data["model"]
+        year = data["year"]
 
         # Get the cursor
         cur = db.connection.cursor()
@@ -39,12 +42,16 @@ def add_car():
 
         db.connection.commit()
 
-        flash('Car added successfully!')
-    return redirect(url_for('Index'))
+        #flash('Car added successfully!')
+    return redirect(url_for("index"))
 
-@app.route('/delete_car/:id')
+@app.route('/update_car')
+def update_car():
+    return redirect(url_for(index))
+
+@app.route('/delete_car')
 def delete_car():
-    return 'Deleting car'
+    return redirect(url_for(index))
 
 # Run the server if the executed file is the main file
 if __name__ == '__main__':
